5822f4faad44d9ab6188ba36ab7a0e01f852ba88
Deven Fan <devenfan@gmail.com>
2014-04-11 15:35:50 +0800

    Hide those MBeans that have java.lang.IllegalStateException: JBAS019905
    Signed-off-by: Deven Fan <devenfan@gmail.com>

--- src/main/java/org/jboss/jmx/adaptor/control/Server.java
+++ src/main/java/org/jboss/jmx/adaptor/control/Server.java
@@ -82,6 +82,10 @@ public class Server {
                         domainData.put(domainName, data);
                     }
                     data.addData(mbeanData);
+                } catch (IllegalStateException ex1) {
+                	// java.lang.IllegalStateException: JBAS019905
+                	log.warn("Cannot get mbean info of: " + name);
+                	log.warn("java.lang.IllegalStateException: " + ex1.getMessage());
                 } catch (Exception ex) {
                 	log.error("Issue happened when get mbean info of: " + name, ex);
                 }
